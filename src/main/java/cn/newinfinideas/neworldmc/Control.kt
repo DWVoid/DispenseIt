6168a6187d88bc00bd7b6d2b2358ec045a7bb142
DWVoid <yshliu0321@icloud.com>
2020-08-07 18:29:06 +0800

    more functions

--- src/main/java/cn/newinfinideas/neworldmc/Control.kt
+++ src/main/java/cn/newinfinideas/neworldmc/Control.kt
@@ -4,33 +4,77 @@ import org.bukkit.Bukkit
 import org.bukkit.ChatColor
 import org.bukkit.Material
 import org.bukkit.NamespacedKey
-import org.bukkit.block.Container
 import org.bukkit.enchantments.Enchantment
-import org.bukkit.event.EventHandler
-import org.bukkit.event.Listener
-import org.bukkit.event.block.BlockDispenseEvent
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.ShapedRecipe
+import org.bukkit.inventory.ShapelessRecipe
 import org.bukkit.inventory.meta.EnchantmentStorageMeta
 import org.bukkit.plugin.Plugin
 
-
 fun makeInfiniteItemRecipe(
-    parent: Plugin, // parent plugin
-    item: ItemStack // The item involved
+        parent: Plugin, // parent plugin
+        item: ItemStack // The item involved
 ): NamespacedKey {
+    val newItem = addInfinite8(item)
+    val key = NamespacedKey(parent, "infinite_${newItem.type.name}")
+    Bukkit.addRecipe(ShapedRecipe(key, newItem)
+            .shape("EEE", "ESE", "EEE").setIngredient('S', item)
+            .setIngredient('E', getEnchantmentBook(Enchantment.ARROW_INFINITE, 1))
+    )
+    return key
+}
+
+private fun addInfinite8(item: ItemStack): ItemStack {
     val newItem = item.clone()
     newItem.addUnsafeEnchantment(Enchantment.ARROW_INFINITE, 8)
     val meta = newItem.itemMeta
-    meta.setDisplayName(ChatColor.GREEN.toString() + item.i18NDisplayName)
+    meta.setDisplayName("${ChatColor.GREEN}${item.i18NDisplayName}")
     newItem.itemMeta = meta
-    val key = NamespacedKey(parent, "infinite_" + newItem.type.name)
-    val recipe = ShapedRecipe(key, newItem)
-    recipe.shape("EEE", "ESE", "EEE")
-    recipe.setIngredient('E', getEnchantmentBook(Enchantment.ARROW_INFINITE, 1))
-    recipe.setIngredient('S', item)
-    Bukkit.addRecipe(recipe)
+    return newItem
+}
+
+fun makeExpensiveInfiniteItemRecipe(parent: Plugin, item: ItemStack, core: ItemStack): NamespacedKey {
+    val newItem = addInfinite8(item)
+    val key = NamespacedKey(parent, "infinite_${newItem.type.name}")
+    Bukkit.addRecipe(ShapelessRecipe(key, newItem).addIngredient(item).addIngredient(core))
     return key
+}
+
+private fun makeExpensiveInfiniteCraftingCorePartial(name: String): ItemStack {
+    val item = ItemStack(Material.NETHER_STAR)
+    item.addUnsafeEnchantment(Enchantment.ARROW_INFINITE, 7)
+    val meta = item.itemMeta
+    meta.setDisplayName("${ChatColor.GREEN}$name")
+    item.itemMeta = meta
+    return item
+}
+
+private fun makeExpensiveInfiniteCraftingCore(name: String): ItemStack {
+    val item = ItemStack(Material.NETHER_STAR)
+    item.addUnsafeEnchantment(Enchantment.ARROW_INFINITE, 42)
+    val meta = item.itemMeta
+    meta.setDisplayName("${ChatColor.GOLD}$name")
+    item.itemMeta = meta
+    return item
+}
+
+fun makeExpensiveInfiniteCraftingCoreCraftingRecipe(parent: Plugin, partialName: String, name: String): ItemStack {
+    val partialCore = makeExpensiveInfiniteCraftingCorePartial(partialName)
+    val core = makeExpensiveInfiniteCraftingCore(name)
+    val keyPart = NamespacedKey(parent, "infinite_core_partial")
+    val keyFull = NamespacedKey(parent, "infinite_core")
+    val stack = ItemStack(Material.CHORUS_FRUIT, 16)
+    Bukkit.addRecipe(ShapedRecipe(keyPart, partialCore)
+            .shape(" E ", "ESE", " E ").setIngredient('S', stack)
+            .setIngredient('E', getEnchantmentBook(Enchantment.ARROW_INFINITE, 1))
+    )
+    Bukkit.addRecipe(ShapedRecipe(keyFull, core).shape(" E ", "EEE", " E ").setIngredient('E', partialCore))
+    return core
+}
+
+fun removeExpensiveInfiniteCraftingCoreCraftingRecipe(parent: Plugin) {
+    Bukkit.removeRecipe(NamespacedKey(parent, "infinite_core_partial"))
+    Bukkit.removeRecipe(NamespacedKey(parent, "infinite_core"))
 }
 
 @Suppress("SameParameterValue")
@@ -41,4 +85,3 @@ private fun getEnchantmentBook(enchant: Enchantment, level: Int): ItemStack {
     book.itemMeta = meta
     return book
 }
-
--- src/main/java/cn/newinfinideas/neworldmc/DispenseIt.kt
+++ src/main/java/cn/newinfinideas/neworldmc/DispenseIt.kt
@@ -1,9 +1,15 @@
 package cn.newinfinideas.neworldmc
 
 import org.bukkit.Bukkit
+import org.bukkit.Location
 import org.bukkit.Material
 import org.bukkit.NamespacedKey
+import org.bukkit.block.BlockFace
 import org.bukkit.block.Container
+import org.bukkit.block.data.Directional
+import org.bukkit.block.Dispenser
+import org.bukkit.block.data.type.Slab
+import org.bukkit.block.data.type.Stairs
 import org.bukkit.enchantments.Enchantment
 import org.bukkit.event.EventHandler
 import org.bukkit.event.HandlerList
@@ -33,20 +39,30 @@ class DispenseIt : JavaPlugin(), Listener {
     private fun prepareInfiniteItems() {
         val conf = this.config
         val listInfinite = conf.getStringList("lists.infinite")
+        val listExpensiveInfinite = conf.getStringList("lists.expensive_infinite")
+        val core = makeExpensiveInfiniteCraftingCoreCraftingRecipe(
+                this, config.getString("names.expensive_core_partial")!!, config.getString("names.expensive_core")!!
+        )
         val unregisterList = mutableListOf<NamespacedKey>()
         for (name in listInfinite) {
             Material.getMaterial(name)?.let { unregisterList.add(makeInfiniteItemRecipe(this, ItemStack(it))) }
         }
+        for (name in listExpensiveInfinite) {
+            Material.getMaterial(name)?.let { unregisterList.add(makeExpensiveInfiniteItemRecipe(this, ItemStack(it), core)) }
+        }
         recipeUnloadList = unregisterList.toTypedArray()
     }
 
+
     private fun unloadInfiniteItems() {
         for (key in recipeUnloadList!!) Bukkit.removeRecipe(key)
+        removeExpensiveInfiniteCraftingCoreCraftingRecipe(this)
+        recipeUnloadList = null
     }
 
     override fun onDisable() {
-        unloadInfiniteItems()
         HandlerList.unregisterAll(this as Plugin)
+        unloadInfiniteItems()
         super.onDisable()
     }
 
@@ -54,18 +70,80 @@ class DispenseIt : JavaPlugin(), Listener {
     fun onDispenserFire(event: BlockDispenseEvent) {
         // Note: Originally we want both dispensers and droppers to have this behaviour
         //     but the dispenser event handling have pretty wacky behaviours, so now we will only allow droppers
-        if (event.block.type == Material.DROPPER) {
-            val now = event.item
-            if (now.enchantments[Enchantment.ARROW_INFINITE] != 8) return
-            val addBack = now.asOne()
-            val inventory = (event.block.getState(false) as Container).inventory
-            server.scheduler.scheduleSyncDelayedTask(this) { inventory.addItem(addBack) }
-            event.item = prepareItemCommonDuplication(now)
+        when (event.block.type) {
+            Material.DROPPER -> tryDupeItemAction(event)
+            Material.DISPENSER -> tryPlaceBlockAction(event)
+            else->{}
         }
     }
 
+    private fun tryPlaceBlockAction(event: BlockDispenseEvent) {
+        val block = event.block
+        val dispenser = block.getState(false) as Dispenser
+        if (!checkBlockPlacementInventoryPattern(dispenser.inventory.contents)) return
+        event.isCancelled = true
+        val directional = block.getState(false).blockData as Directional
+        val location = block.location.add(directional.facing.direction)
+        server.scheduler.scheduleSyncDelayedTask(this) { tryPutBlockDispenser(dispenser, location, directional) }
+    }
+
+    private fun tryPutBlockDispenser(dispenser: Dispenser, location: Location, directional: Directional) {
+        if (!dispenser.isPlaced) return
+        val stack = dispenser.inventory.contents[4] ?: return
+        if (!stack.type.isBlock) return
+        if (stack.type == Material.REDSTONE_BLOCK) return // REDSTONE_BLOCK causes bug
+        if (!checkBlockPlacementInventoryPattern(dispenser.inventory.contents)) return
+        if (stack.amount > 0) {
+            val blockAt = dispenser.world.getBlockAt(location)
+            if (blockAt.type.isAir) {
+                blockAt.type = stack.type
+                val blockAtState = blockAt.blockData
+                when (blockAtState) {
+                    is Directional -> blockAtState.facing = directional.facing
+                    is Slab -> blockAtState.type = getSlabType(directional.facing)
+                }
+                blockAt.blockData = blockAtState
+                stack.subtract()
+            }
+        }
+    }
+
+    private fun getSlabType(facing: BlockFace): Slab.Type {
+        return when (facing) {
+            BlockFace.DOWN -> Slab.Type.TOP
+            else -> Slab.Type.BOTTOM
+        }
+    }
+
+    private fun tryDupeItemAction(event: BlockDispenseEvent) {
+        val now = event.item
+        if (now.enchantments[Enchantment.ARROW_INFINITE] != 8) return
+        val addBack = now.asOne()
+        val inventory = (event.block.getState(false) as Container).inventory
+        server.scheduler.scheduleSyncDelayedTask(this) { inventory.addItem(addBack) }
+        event.item = prepareItemCommonDuplication(now)
+    }
+
+    private fun checkBlockPlacementInventoryPattern(stacks: Array<ItemStack?>): Boolean {
+        if (!checkPatternSingleStack(stacks[0])) return false
+        if (!checkPatternSingleStack(stacks[1])) return false
+        if (!checkPatternSingleStack(stacks[2])) return false
+        if (!checkPatternSingleStack(stacks[3])) return false
+        if (!checkPatternSingleStack(stacks[5])) return false
+        if (!checkPatternSingleStack(stacks[6])) return false
+        if (!checkPatternSingleStack(stacks[7])) return false
+        if (!checkPatternSingleStack(stacks[8])) return false
+        return true
+    }
+
+    private fun checkPatternSingleStack(stacks: ItemStack?): Boolean {
+        if (stacks == null) return false
+        if (stacks.type != Material.DROPPER) return false
+        return true
+    }
+
     @EventHandler
-    fun onInventoryMode(event: InventoryMoveItemEvent) {
+    fun onInventoryMoveItem(event: InventoryMoveItemEvent) {
         val inventory = event.initiator
         if (inventory.type == InventoryType.DROPPER) {
             val now = event.item
@@ -78,7 +156,7 @@ class DispenseIt : JavaPlugin(), Listener {
         val next = now.clone()
         next.removeEnchantment(Enchantment.ARROW_INFINITE)
         val meta = next.itemMeta
-        meta.setDisplayName(next.i18NDisplayName)
+        meta.setDisplayName(null)
         next.itemMeta = meta
         return next
     }
